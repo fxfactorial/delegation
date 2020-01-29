b649f0aecc84ffca9678af529e44cd540707eacd
Edgar Aroutiounian <edgar.factorial@gmail.com>
2020-01-28 20:18:20 -0800

    [project] Kick off RPC call directly, pull in method names of possible interest

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,36 @@
 const functions = require('firebase-functions');
+const { Harmony } = require('@harmony-js/core');
+const { ChainID, ChainType } = require('@harmony-js/utils');
+const fetch = require('node-fetch');
+const methods = require('./methods');
 
-const a = 10;
+const { Headers } = fetch;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const url = 'http://18.212.21.97:9500';
+
+const harmony = new Harmony(url, {
+  chainType: ChainType.Harmony,
+  chainId: ChainID.HmyTestnet,
+});
+
+const headers = new Headers({
+  accept: 'application/json',
+  'Content-type': 'application/json',
+});
+
+const jsonrpc = '2.0';
+
+const id = 0;
+
+const hmy_query = (method, params = []) =>
+  fetch(url, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ id: 0, method, params }),
+  }).then(resp => resp.json());
+
+exports['stake'] = functions.https.onRequest(async (request, response) => {
+  const result = await hmy_query(methods.GetActiveValidatorAddresses);
+  console.log(result);
+  response.send('Hello from Firebase!');
+});
---
+++ functions/methods.js
@@ -0,0 +1,10 @@
+module.exports = {
+  GetActiveValidatorAddresses: 'hmy_getActiveValidatorAddresses',
+  GetAllValidatorAddresses: 'hmy_getAllValidatorAddresses',
+  GetValidatorInformation: 'hmy_getValidatorInformation',
+  GetDelegationsByDelegator: 'hmy_getDelegationsByDelegator',
+  GetDelegationsByValidator: 'hmy_getDelegationsByValidator',
+  GetCurrentTransactionErrorSink: 'hmy_getCurrentTransactionErrorSink',
+  GetValidatorMetrics: 'hmy_getValidatorMetrics',
+  GetMedianRawStakeSnapshot: 'hmy_getMedianRawStakeSnapshot',
+};
